37c2f90db73010485674867b898cac3ed5d3b103
Courtney Shearer <csheare@clemson.edu>
2017-11-18 17:46:17 -0500

    integrate with pi

--- flask/app.py
+++ flask/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template, json, request
+import RPi.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN)
+GPIO.setwarnings(False)
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def main():
@@ -10,6 +15,20 @@ def main():
 @app.route('/jam/', methods=['POST','GET'])
 def jam():
     sound = request.form.get('sound')
+
+    while True:
+        if(GPIO.input(23) == False):
+            print("stuff happening")
+            if sound == Airplane:
+                choice = 'sounds/airplane.mp3'
+            elif sound == Boomerang:
+                choice = 'sounds/Boomerang.mp3'
+            else:
+                choice = 'sounds/marbles.mp3'
+
+            os.system('mpg123 -q ' + choice + ' &')
+        sleep(0.1);
+
     return render_template('jam.html',**locals())
 
 if __name__ == "__main__":
