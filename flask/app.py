7e7f4939f350ea1a0f9050c4303488745a3dee13
Courtney Shearer <csheare@clemson.edu>
2017-11-19 20:40:29 +0000

    It works kids

--- flask/app.py
+++ flask/app.py
@@ -1,39 +1,45 @@
 from flask import Flask, render_template, json, request
-# import RPi.GPIO as GPIO
-# from time import sleep
+import RPi.GPIO as GPIO
+from time import sleep
+import os
+import _thread
 
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setup(23, GPIO.IN)
-# GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23, GPIO.IN)
+GPIO.setwarnings(False)
 
-app = Flask(__name__)
+flask_app = Flask(__name__)
 
-choice = "Marbles"
+choice = ""
 
-@app.route('/')
+@flask_app.route('/')
 def main():
     return render_template('index.html')
 
-@app.route('/jam/', methods=['POST','GET'])
+@flask_app.route('/jam/', methods=['POST','GET'])
 def jam():
+    global choice
     sound = request.form.get('sound')
     print("stuff happening")
     if sound == 'Airplane':
-        choice = 'sounds/airplane.mp3'
+        choice = 'static/sounds/airplane.mp3'
     elif sound == 'Boomerang':
-        choice = 'sounds/Boomerang.mp3'
-    else:
-        choice = 'sounds/marbles.mp3'
-
+        choice = 'static/sounds/Boomerang.mp3'
+    elif sound == 'Marbles':
+        choice = 'static/sounds/marbles.mp3'
+	
     return render_template('jam.html',**locals())
 
-@app.route('/jam/temp')
 def temp():
     while True:
+    #print("YO")
         if(GPIO.input(23) == False):
             os.system('mpg123 -q ' + choice + ' &')
-        sleep(0.1);
-    return render_template('jam.html',**locals())
+        sleep(0.1)
+    #return 1
+ 
+	
 
 if __name__ == "__main__":
-    app.run()
+    _thread.start_new_thread(temp,())
+    flask_app.run(host='198.21.247.207')
