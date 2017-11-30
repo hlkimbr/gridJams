e706830eb7425abe508f531068c3f245565a5bf3
Courtney Shearer <csheare@clemson.edu>
2017-11-30 17:36:53 +0000

    its works

--- flask/app.py
+++ flask/app.py
@@ -6,16 +6,21 @@ import _thread
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(23, GPIO.IN)
-GPIO.setwarnings(False)
+GPIO.setup(24, GPIO.IN)
+GPIO.setup(25, GPIO.IN)
+GPIO.setup(12, GPIO.IN)
+GPIO.setup(16, GPIO.IN)
+GPIO.setup(20, GPIO.IN)
+#GPIO.setwarnings(False)
 
 flask_app = Flask(__name__)
 
-button1 = ""
-button2 = ""
-button3 = ""
-button4 = ""
-button5 = ""
-button6 = ""
+button1 = ""#"static/sounds/aaahMadeon.mp3"
+button2 = ""#"static/sounds/clap.mp3"
+button3 = ""#"static/sounds/freezeIntroAudio.mp3"
+button4 = ""#"static/sounds/mario.mp3"
+button5 = ""#"static/sounds/midifreeze.mp3"
+button6 = ""#static/sounds/over.mp3"
 
 @flask_app.route('/')
 def main():
@@ -23,30 +28,35 @@ def main():
 
 @flask_app.route('/jam/', methods=['POST','GET'])
 def jam():
-    global choice
+    global button1
+    global button2
+    global button3
+    global button4
+    global button5
+    global button6
     sound = request.form.get('sound')
     print("stuff happening")
     if sound == 'Theme1':
-        button1 = "static/sounds/aaahMadeon"
+        button1 = "static/sounds/aaahMadeon.mp3"
         button2 = "static/sounds/clap.mp3"
         button3 = "static/sounds/freezeIntroAudio.mp3"
         button4 = "static/sounds/mario.mp3"
         button5 = "static/sounds/midifreeze.mp3"
         button6 = "static/sounds/over.mp3"
     elif sound == 'Theme2':
-        button1 = ""
-        button2 = ""
-        button3 = ""
-        button4 = ""
-        button5 = ""
-        button6 = ""
+        button1 = "static/sounds/Boomerang.mp3"
+        button2 = "static/sounds/clap.mp3"
+        button3 = "static/sounds/over.mp3"
+        button4 = "static/sounds/pianoBreak.mp3"
+        button5 = "static/sounds/slice44.mp3"
+        button6 = "static/sounds/aaahMadeon.mp3"
     elif sound == 'Theme3':
-        button1 = ""
-        button2 = ""
-        button3 = ""
-        button4 = ""
-        button5 = ""
-        button6 = ""
+        button1 = "static/sounds/midifreeze.mp3"
+        button2 = "static/sounds/marbles.mp3"
+        button3 = "static/sounds/slice45.mp3"
+        button4 = "static/sounds/slice210.mp3"
+        button5 = "static/sounds/aaahMadeon.mp3"
+        button6 = "static/sounds/over.mp3"
 
     return render_template('jam.html',**locals())
 
@@ -54,26 +64,19 @@ def buttons():
     while True:
         if(GPIO.input(23) == False):
             os.system('mpg123 -q ' + button1 + ' &')
-        sleep(0.1)
         if(GPIO.input(24) == False):
-            os.system('mpg123 -q ' + button2 + ' &')
-        sleep(0.1)
+             os.system('mpg123 -q ' + button2 + ' &')
         if(GPIO.input(25) == False):
-            os.system('mpg123 -q ' + button3 + ' &')
-        sleep(0.1)
+             os.system('mpg123 -q ' + button3 + ' &')
         if(GPIO.input(12) == False):
-            os.system('mpg123 -q ' + button4 + ' &')
-        sleep(0.1)
+             os.system('mpg123 -q ' + button4 + ' &')
         if(GPIO.input(16) == False):
-            os.system('mpg123 -q ' + button5 + ' &')
-        sleep(0.1)
-        if(GPIO.input(23) == False):
-            os.system('mpg123 -q ' + button6 + ' &')
-        sleep(0.1)
-    #return 1
+             os.system('mpg123 -q ' + button5 + ' &')
+        if(GPIO.input(20) == False):
+             os.system('mpg123 -q ' + button6 + ' &')
 
-
+        sleep(0.1);
 
 if __name__ == "__main__":
     _thread.start_new_thread(buttons,())
-    flask_app.run(host='198.21.247.207')
+    flask_app.run(host='198.21.245.75')
